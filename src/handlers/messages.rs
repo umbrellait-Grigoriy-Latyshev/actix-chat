cca5fe0765bdcb9cfd27b6a61ba605e9dd015fc4
Grigory Latyshev <thegriglat@gmail.com>
2022-08-22 16:11:59 +0300

    Return 500 in case of error

--- src/handlers/messages.rs
+++ src/handlers/messages.rs
@@ -1,5 +1,5 @@
 use actix::entities::message::Message;
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{get, http::StatusCode, post, web, HttpResponse, Responder};
 
 use crate::{
     handlers::dto::{PostMessageDto, PostMessageDtoResponse},
@@ -18,18 +18,28 @@ pub async fn post_message(info: web::Json<PostMessageDto>) -> impl Responder {
 
 async fn get_all_messages(
     connection: PooledConnection<ConnectionManager<PgConnection>>,
-) -> Vec<Message> {
+) -> Result<Vec<Message>, diesel::result::Error> {
     use actix::schema::messages::dsl::*;
-    let result = messages
-        .limit(5)
-        .load::<Message>(&connection)
-        .expect("Error loading messages");
+    let result = messages.limit(5).load::<Message>(&connection);
     result
 }
 
 #[get("/messages")]
 pub async fn get_messages(pool: web::Data<DbPool>) -> impl Responder {
-    let connection = pool.get().expect("couldn't get db connection from pool");
+    let connection = match pool.get() {
+        Ok(conn) => conn,
+        Err(_) => {
+            return HttpResponse::Ok()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body("Cannot get connection from pool")
+        }
+    };
     let results = get_all_messages(connection).await;
-    HttpResponse::Ok().json(results)
+    let response = match results {
+        Ok(res) => HttpResponse::Ok().json(res),
+        Err(_) => HttpResponse::Ok()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body("Cannot fetch messages"),
+    };
+    response
 }
