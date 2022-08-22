33e7c5754a0099a82f4f60cb656a1f5e0d4f4ca4
Grigory Latyshev <thegriglat@gmail.com>
2022-08-22 15:51:31 +0300

    User r2d2 pool with actix

--- src/handlers/messages.rs
+++ src/handlers/messages.rs
@@ -1,9 +1,14 @@
 use actix::entities::message::Message;
-use actix::establish_connection;
 use actix_web::{get, post, web, HttpResponse, Responder};
 
-use crate::handlers::dto::{PostMessageDto, PostMessageDtoResponse};
-use diesel::prelude::*;
+use crate::{
+    handlers::dto::{PostMessageDto, PostMessageDtoResponse},
+    DbPool,
+};
+use diesel::{
+    prelude::*,
+    r2d2::{ConnectionManager, PooledConnection},
+};
 
 #[post("/message")]
 pub async fn post_message(info: web::Json<PostMessageDto>) -> impl Responder {
@@ -11,16 +16,20 @@ pub async fn post_message(info: web::Json<PostMessageDto>) -> impl Responder {
     HttpResponse::Ok().json(PostMessageDtoResponse::new())
 }
 
-#[get("/messages")]
-pub async fn get_messages() -> impl Responder {
+async fn get_all_messages(
+    connection: PooledConnection<ConnectionManager<PgConnection>>,
+) -> Vec<Message> {
     use actix::schema::messages::dsl::*;
-
-    let connection = establish_connection();
-    let results = messages
+    let result = messages
         .limit(5)
         .load::<Message>(&connection)
         .expect("Error loading messages");
+    result
+}
 
-    println!("messages length: {}", results.len());
+#[get("/messages")]
+pub async fn get_messages(pool: web::Data<DbPool>) -> impl Responder {
+    let connection = pool.get().expect("couldn't get db connection from pool");
+    let results = get_all_messages(connection).await;
     HttpResponse::Ok().json(results)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 
 use actix_web::middleware::Logger;
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::PgConnection;
 use dotenv::dotenv;
 use env_logger::Env;
 
 mod handlers;
 use std::env;
 
+pub type DbPool = Pool<ConnectionManager<PgConnection>>;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-
+    // logger
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    // r2d2 pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::new(manager).expect("Cannot create r2d2 pool");
 
     let host = env::var("HOST").expect("HOST variable not set!");
     let port = env::var("PORT")
@@ -21,8 +30,10 @@ async fn main() -> std::io::Result<()> {
 
     println!("Chat server run at http://{}:{}", host, port.to_string());
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            // add r2d2 pool
+            .app_data(Data::new(pool.clone()))
             // logging
             .wrap(Logger::new("%a %{User-Agent}i %r %s, %T secs"))
             // API
