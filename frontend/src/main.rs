a8166b094673c68325311b15798854890a54e3a7
Grigory Latyshev <thegriglat@gmail.com>
2022-08-23 22:09:56 +0300

    Try to use Yew

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,46 @@
+use serde::Deserialize;
 use yew::prelude::*;
+
+#[derive(Clone, PartialEq, Deserialize)]
+struct Message {
+    actor: i32,
+    text: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct MessageListProps {
+    messages: Vec<Message>,
+}
+
+#[function_component(MessageList)]
+fn message_list(MessageListProps { messages }: &MessageListProps) -> Html {
+    messages
+        .iter()
+        .map(|message| {
+            html! {
+                <p>{format!("{}", message.text)}</p>
+            }
+        })
+        .collect()
+}
 
 #[function_component(App)]
 fn app() -> Html {
+    let messages = vec![
+        Message {
+            actor: 1,
+            text: "test 1".to_string(),
+        },
+        Message {
+            actor: 2,
+            text: "text 2".to_string(),
+        },
+    ];
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "Simple Rust chat" }</h1>
+            <MessageList messages={messages} />
+        </>
     }
 }
 
