00df5a1fc6cbcb46d7cf0fb13048e970f3de90d7
Grigory Latyshev <thegriglat@gmail.com>
2022-08-28 16:22:34 +0300

    Refactor

--- frontend/src/components/message_list.rs
+++ frontend/src/components/message_list.rs
@@ -1,15 +1,29 @@
-use yew::{function_component, html, Properties};
+use wasm_bindgen_futures::spawn_local;
+use yew::{function_component, html, use_effect, use_state, Properties};
 
 use super::chat_message::ChatMessage;
 use crate::api::Message;
 
 #[derive(Properties, PartialEq, Eq)]
-pub struct MessageListProps {
-    pub messages: Vec<Message>,
-}
+pub struct MessageListProps;
 
 #[function_component(MessageList)]
-pub fn message_list(MessageListProps { messages }: &MessageListProps) -> Html {
+pub fn message_list(_props: &MessageListProps) -> Html {
+    let messages = use_state(Vec::<Message>::new);
+
+    {
+        let messages = messages.clone();
+        use_effect(move || {
+            let messages = messages;
+
+            spawn_local(async move {
+                let fetched = crate::api::api_messages().await.unwrap();
+                messages.set(fetched);
+            });
+            || ()
+        });
+    }
+
     messages
         .iter()
         .map(|message| {
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,34 +1,15 @@
-use wasm_bindgen_futures::spawn_local;
-use yew::prelude::*;
-
 use crate::components::message_list::MessageList;
+use yew::prelude::*;
 
 mod api;
 mod components;
 
 #[function_component(App)]
 fn app() -> Html {
-    let messages = use_state(Vec::new);
-    {
-        let messages = messages.clone();
-        use_effect_with_deps(
-            move |_| {
-                let messages = messages;
-
-                spawn_local(async move {
-                    let fetched = api::api_messages().await.unwrap();
-                    messages.set(fetched);
-                });
-                || ()
-            },
-            (),
-        );
-    }
-
     html! {
         <>
         <components::header::Header/>
-            <MessageList messages={(*messages).clone()} />
+            <MessageList  />
         </>
     }
 }
